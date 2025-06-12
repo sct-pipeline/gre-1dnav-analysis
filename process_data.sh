79f931539d62026f9ed697eeeacc7977ee83c73a
jcohenadad <jcohen@polymtl.ca>
2025-06-12 15:54:45 -0400

    Added code to loop across regions

--- process_data.sh
+++ process_data.sh
@@ -34,6 +34,27 @@ start=`date +%s`
 # FUNCTIONS
 # ==============================================================================
 
+# Check if manual segmentation already exists. If it does, copy it locally. If
+# it does not, perform seg.
+segment_if_does_not_exist(){
+  local file="$1"
+  local contrast="$2"
+  # Update global variable with segmentation file name
+  FILESEG="${file}_seg"
+  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILESEG}-manual$EXT"
+  echo
+  echo "Looking for manual segmentation: $FILESEGMANUAL"
+  if [[ -e $FILESEGMANUAL ]]; then
+    echo "Found! Using manual segmentation."
+    rsync -avzh $FILESEGMANUAL ${FILESEG}$EXT
+    sct_qc -i ${file}$EXT -s ${FILESEG}$EXT -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  else
+    echo "Not found. Proceeding with automatic segmentation."
+    # Segment spinal cord
+    sct_deepseg spinalcord -i ${file}$EXT -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  fi
+}
+
 # Check if manual label already exists. If it does, copy it locally. If it does
 # not, perform labeling.
 label_if_does_not_exist(){
@@ -41,47 +62,21 @@ label_if_does_not_exist(){
   local file_seg="$2"
   # Update global variable with segmentation file name
   FILELABEL="${file}_labels"
-  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual.nii.gz"
+  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual$EXT"
   echo "Looking for manual label: $FILELABELMANUAL"
   if [[ -e $FILELABELMANUAL ]]; then
     echo "Found! Using manual labels."
-    rsync -avzh $FILELABELMANUAL ${FILELABEL}.nii.gz
+    rsync -avzh $FILELABELMANUAL ${FILELABEL}$EXT
   else
     echo "Not found. Proceeding with automatic labeling."
     # Generate labeled segmentation
-    sct_deepseg totalspineseg -i ${file}.nii.gz
-    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -c t1 -discfile ${file}_step1_levels.nii.gz
+    sct_deepseg totalspineseg -i ${file}$EXT
+    sct_label_vertebrae -i ${file}$EXT -s ${file_seg}$EXT -c t1 -discfile ${file}_step1_levels$EXT
     # Create labels in the cord at C3 and C5 mid-vertebral levels
-    sct_label_utils -i ${file_seg}_labeled.nii.gz -vert-body 3,7 -o ${FILELABEL}.nii.gz
+    sct_label_utils -i ${file_seg}_labeled$EXT -vert-body 3,7 -o ${FILELABEL}$EXT
   fi
 }
 
-# Check if manual segmentation already exists. If it does, copy it locally. If
-# it does not, perform seg.
-segment_if_does_not_exist(){
-  local file="$1"
-  local contrast="$2"
-  # Find contrast
-  if [[ $contrast == "dwi" ]]; then
-    folder_contrast="dwi"
-  else
-    folder_contrast="anat"
-  fi
-  # Update global variable with segmentation file name
-  FILESEG="${file}_seg"
-  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/${folder_contrast}/${FILESEG}-manual.nii.gz"
-  echo
-  echo "Looking for manual segmentation: $FILESEGMANUAL"
-  if [[ -e $FILESEGMANUAL ]]; then
-    echo "Found! Using manual segmentation."
-    rsync -avzh $FILESEGMANUAL ${FILESEG}.nii.gz
-    sct_qc -i ${file}.nii.gz -s ${FILESEG}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
-  else
-    echo "Not found. Proceeding with automatic segmentation."
-    # Segment spinal cord
-    sct_deepseg spinalcord -i ${file}.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT}
-  fi
-}
 
 
 # SCRIPT STARTS HERE
@@ -105,19 +100,33 @@ rsync -avzh $PATH_DATA/$SUBJECT .
 # Go to anat folder where all structural data are located
 cd ${SUBJECT}/anat/
 
-
 CONTRAST="T2starw"
+EXT="nii.gz"
 
-file_data="${SUBJECT}_$CONTRAST"
+# Create a list of values with 'acq-' in the file name
+FILES_ACQ=("lowerT" "upperT" "LSE")
 
-# Segment spinal cord (only if it does not exist)
-segment_if_does_not_exist $file_data
-file_seg=$FILESEG
+# Loop across FILES_ACQ
+for acq in ${FILES_ACQ[@]}; do
+  # Start processing the navitor data
+  file_data="${SUBJECT}_acq-$acq_rec-navigated_$CONTRAST"
+  echo "Processing: $file_data$EXT"
+  # Check if the file exists
+  if [[ ! -e $file_data$EXT ]]; then
+    echo "File $file_data$EXT does not exist. Skipping."
+    continue
+  fi
+  # Segment spinal cord (only if it does not exist)
+  segment_if_does_not_exist $file_data
+  file_seg=$FILESEG
+done
+
 
 # Verify presence of output files and write log file if error
 # ------------------------------------------------------------------------------
 FILES_TO_CHECK=(
-  "anat/${SUBJECT}_T2starw_seg.nii.gz"
+  "anat/${SUBJECT}_acq-$acq_rec-navigated_$CONTRAST$EXT"
+  "anat/${SUBJECT}_acq-$acq_rec-navigated_${CONTRAST_seg$EXT"
 )
 for file in ${FILES_TO_CHECK[@]}; do
   if [[ ! -e $file ]]; then
