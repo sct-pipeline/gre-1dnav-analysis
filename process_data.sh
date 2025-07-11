37ec7a6737bbd77eb9d701843fb47b133ebe919c
AntoineGuenette <anto.guenette@gmail.com>
2025-07-11 18:23:08 -0400

    Reduce the number of lines defining how the WM segmentation is named

--- process_data.sh
+++ process_data.sh
@@ -102,65 +102,58 @@ segment_gm_if_does_not_exist(){
 
 
 compute_wm_if_does_not_exist(){
-   local file="$1"
-   local file_seg="$2"
-   local file_gmseg="$3"
-   FILESEG="${file}_wmseg"
-   PATHSEG="${PATH_DATA}/derivatives/labels/${SUBJECT_SLASH_SESSION}/anat/${FILESEG}$EXT"
-   if [[ -e $PATHSEG ]]; then
-      echo "Found! Using segmentation $PATHSEG"
-      rsync -avzh $PATHSEG ${file}_wmseg$EXT
-   else
-      echo "Not found. Calculate WM mask."
-      sct_maths -i ${file_seg}${EXT} -sub ${file_gmseg}${EXT} -o $PATHSEG
-      rsync -avzh $PATHSEG ${file}_wmseg$EXT
+  local file="$1"
+  local file_seg="$2"
+  local file_gmseg="$3"
+  FILESEG="${file}_wmseg"
+  PATHSEG="${PATH_DATA}/derivatives/labels/${SUBJECT_SLASH_SESSION}/anat/${FILESEG}$EXT"
+  if [[ -e $PATHSEG ]]; then
+    echo "Found! Using segmentation $PATHSEG"
+    rsync -avzh $PATHSEG ${FILESEG}${EXT}
+  else
+    echo "Not found. Calculate WM mask."
+    sct_maths -i ${file_seg}${EXT} -sub ${file_gmseg}${EXT} -o $PATHSEG
+    rsync -avzh $PATHSEG ${FILESEG}${EXT}
    fi
 }
 
 
 compute_snr_cnr(){
-   local file="$1"
-   local file_wmseg="$2"
-   local file_gmseg="$3"
-
-   # Split images, gm, and wm segs into individual slices to calculate SNR/CNR for each slice
-   PATH_TMP=${PATH_DATA_PROCESSED}/tmp
-   PATH_TMP_SUBJECT=${PATH_TMP}/${SUBJECT_SLASH_SESSION}
-   mkdir -p ${PATH_TMP_SUBJECT}
-   sct_image -i ${file_gmseg}${EXT} -split z -o "${PATH_TMP_SUBJECT}/${file_gmseg}${EXT}"
-   sct_image -i ${file_wmseg}${EXT} -split z -o "${PATH_TMP_SUBJECT}/${file}_wmseg${EXT}"
-   sct_image -i ${file}${EXT} -split z -o "${PATH_TMP_SUBJECT}/${file}${EXT}"
-   file_gmseg_ind="${PATH_TMP_SUBJECT}/${file_gmseg}"
-   file_wmseg_ind="${PATH_TMP_SUBJECT}/${file_wmseg}"
-   file_ind="${PATH_TMP_SUBJECT}/${file}"
-
-   # Find the number of slices in image
-   num_slices=$(sct_image -i ${file}${EXT} -header fslhd | grep ^dim3 | awk '{print $2}')
-   num_slices=$((num_slices-1))
-
-
-   mkdir -p ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}
-   mkdir -p ${PATH_RESULTS}/CNR/${SUBJECT_SLASH_SESSION}
-
-   # Calculate SNR/CNR for each slice
-   echo "slice,ID,wm_mean,gm_mean,wm_std,CNR" > ${PATH_RESULTS}/CNR/${SUBJECT_SLASH_SESSION}/${file}_results.csv
-   echo "slice,ID,wm_mean,wm_std,SNR" > ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_wm_results.csv
-   echo "slice,ID,gm_mean,gm_std,SNR" > ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_gm_results.csv
-   for g in $(seq -w 0 ${num_slices}); do
-
-     gmM=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_gmseg_ind}_Z00${g}${EXT} -M)
-     gmS=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_gmseg_ind}_Z00${g}${EXT} -S)
-     wmM=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_wmseg_ind}_Z00${g}${EXT} -M)
-     wmS=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_wmseg_ind}_Z00${g}${EXT} -S)
-     CNR=$(echo "scale=5; ($wmM - $gmM)/ $wmS" | bc)
-     SNR_wm=$(echo "scale=5; $wmM/$wmS" | bc)
-     SNR_gm=$(echo "scale=5; $gmM/$gmS" | bc)
-     echo ${g}","${file_ind}","$wmM","$gmM","$wmS","$CNR >> ${PATH_RESULTS}/CNR/${SUBJECT_SLASH_SESSION}/${file}_results.csv
-     echo ${g}","${file_ind}","$wmM","$wmS","$SNR_wm >> ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_wm_results.csv
-     echo ${g}","${file_ind}","$gmM","$gmS","$SNR_gm >> ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_gm_results.csv
-
-   done
-   rm -r ${PATH_TMP}
+  local file="$1"
+  local file_wmseg="$2"
+  local file_gmseg="$3"
+  # Split images, gm, and wm segs into individual slices to calculate SNR/CNR for each slice
+  PATH_TMP=${PATH_DATA_PROCESSED}/tmp
+  PATH_TMP_SUBJECT=${PATH_TMP}/${SUBJECT_SLASH_SESSION}
+  mkdir -p ${PATH_TMP_SUBJECT}
+  sct_image -i ${file_gmseg}${EXT} -split z -o "${PATH_TMP_SUBJECT}/${file_gmseg}${EXT}"
+  sct_image -i ${file_wmseg}${EXT} -split z -o "${PATH_TMP_SUBJECT}/${file}_wmseg${EXT}"
+  sct_image -i ${file}${EXT} -split z -o "${PATH_TMP_SUBJECT}/${file}${EXT}"
+  file_gmseg_ind="${PATH_TMP_SUBJECT}/${file_gmseg}"
+  file_wmseg_ind="${PATH_TMP_SUBJECT}/${file_wmseg}"
+  file_ind="${PATH_TMP_SUBJECT}/${file}"
+  # Find the number of slices in image
+  num_slices=$(sct_image -i ${file}${EXT} -header fslhd | grep ^dim3 | awk '{print $2}')
+  num_slices=$((num_slices-1))
+  # Calculate SNR/CNR for each slice
+  mkdir -p ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}
+  mkdir -p ${PATH_RESULTS}/CNR/${SUBJECT_SLASH_SESSION}
+  echo "slice,ID,wm_mean,gm_mean,wm_std,CNR" > ${PATH_RESULTS}/CNR/${SUBJECT_SLASH_SESSION}/${file}_results.csv
+  echo "slice,ID,wm_mean,wm_std,SNR" > ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_wm_results.csv
+  echo "slice,ID,gm_mean,gm_std,SNR" > ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_gm_results.csv
+  for g in $(seq -w 0 ${num_slices}); do
+    gmM=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_gmseg_ind}_Z00${g}${EXT} -M)
+    gmS=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_gmseg_ind}_Z00${g}${EXT} -S)
+    wmM=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_wmseg_ind}_Z00${g}${EXT} -M)
+    wmS=$(fslstats ${file_ind}_Z00${g}${EXT} -k ${file_wmseg_ind}_Z00${g}${EXT} -S)
+    CNR=$(echo "scale=5; ($wmM - $gmM)/ $wmS" | bc)
+    SNR_wm=$(echo "scale=5; $wmM/$wmS" | bc)
+    SNR_gm=$(echo "scale=5; $gmM/$gmS" | bc)
+    echo ${g}","${file_ind}","$wmM","$gmM","$wmS","$CNR >> ${PATH_RESULTS}/CNR/${SUBJECT_SLASH_SESSION}/${file}_results.csv
+    echo ${g}","${file_ind}","$wmM","$wmS","$SNR_wm >> ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_wm_results.csv
+    echo ${g}","${file_ind}","$gmM","$gmS","$SNR_gm >> ${PATH_RESULTS}/SNR/${SUBJECT_SLASH_SESSION}/${file}_gm_results.csv
+  done
+  rm -r ${PATH_TMP}
 
 }
 
@@ -243,30 +236,24 @@ EXT=".nii.gz"
 ACQ=("acq-upperT" "acq-lowerT" "acq-LSE")
 REC=("rec-navigated" "rec-standard")
 OVERWRITE_SEG=true
-
 for acq in "${ACQ[@]}";do
   for rec in "${REC[@]}";do
     file=${SUBJECT_UNDERSCORE_SESSION}_${acq}_${rec}_${CONTRAST}
     echo "File: ${file}${EXT}"
     if [ -e "${file}${EXT}" ]; then
       echo "File found! Processing..."
-
       # Always use the navigated segmentation (manually corrected) to calculate SNR/CNR for both standard and navigated images
       segment_if_does_not_exist ${file}
       file_seg=${SUBJECT_UNDERSCORE_SESSION}_${acq}_rec-navigated_${CONTRAST}_label-SC_seg
       segment_gm_if_does_not_exist ${file}
       file_gmseg=${SUBJECT_UNDERSCORE_SESSION}_${acq}_rec-navigated_${CONTRAST}_label-GM_seg
-
       # Calculate WM mask
       compute_wm_if_does_not_exist ${file} ${file_seg} ${file_gmseg}
       file_wmseg="${file}_wmseg"
-
       # Compute slicewise snr and cnr values
       compute_snr_cnr ${file} ${file_wmseg} ${file_gmseg}
-
       # Quantify ghosting
       compute_ghosting "${PATH_DATA}" "${PATH_DATA_PROCESSED}" "${SUBJECT}" "${SESSION}" "${acq}" "${rec}"
-
       # Check if output files exist
       check_if_exists "${acq}" "${rec}"
     else
